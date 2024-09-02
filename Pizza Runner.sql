4d0da7a3afcea7923e0a2a6add75397f0109b64e
Aditya Garg <seedralex@gmail.com>
2024-09-02 20:55:20 +0530

    Update Pizza Runner.sql

--- Pizza Runner.sql
+++ Pizza Runner.sql
@@ -1,1 +1,14 @@
+/*
+Before starting with finding solutions to the questions, let's first address the tables where data is missing or not in favorable condition
+*/
 
+-- Data Cleaning & Transformation
+
+/*
+Table1: pizza_names - no need to clean data
+Table2: pizza_toppings - no need to clean data
+Table3: pizza_recipes - no need to clean data
+Table4: runners - no need to clean data
+Table5: runner_orders - 
+Table6: customer_orders - replaced '' or 'null' with NULL in extras and exclusions table
+*/
